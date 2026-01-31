236c0cdc0c731d62833ca0c2692851a20f8483ec
Bert <bertkdowns@gmail.com>
2026-02-01 01:07:18 +1300

    start render accusations

--- inkle/inkle.ts
+++ inkle/inkle.ts
@@ -70,6 +70,7 @@ class Character {
   story: Story;
   characterImage: string;
   name: string;
+  accused: boolean = false;
   constructor(name: string, image: string) {
     this.characterImage = image;
     this.name = name; // the character name is used to jump to their story path whenever you chat with them.
@@ -181,9 +182,41 @@ export function switchCharacter(characterName: string) {
 
 gameStory.BindExternalFunction("switchCharacter", switchCharacter);
 
+
+export function renderAccusations() {
+  document.getElementById("AccusationModal").style.display = "block";
+  let accusationsDiv = document.getElementById("Accusations");
+  accusationsDiv.innerHTML = "";
+
+
+  for (let charac of Object.values(characters)) {
+    let node = document.createElement("img");
+    node.src = charac.characterImage;
+    node.style.width = "50px";
+    node.style.cursor = "pointer";
+    node.onclick = () => {
+      charac.accused = !charac.accused;
+      renderAccusations();
+    }
+    if (charac.accused) {
+      node.style.border = "2px solid red";
+    }
+    accusationsDiv.appendChild(node);
+
+  }
+
+  document.getElementById("SubmitAccusations").onclick = () => {
+    // TODO: Handle accusations
+  }
+
+
+}
+
+
 export function testrun() {
   modal.state = 0;
   character.chat();
   gameStory.ChoosePathString("CharacterSelection");
   continueStory();
+  renderAccusations();
 }
