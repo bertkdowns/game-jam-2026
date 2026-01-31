12e538b2dddc555791aff041a3db4ed7f165ff62
ChocolateVS <rghthorley@gmail.com>
2026-02-01 01:59:28 +1300

    map characters

--- inkle/constants.ts
+++ inkle/constants.ts
@@ -19,23 +19,23 @@ export const CHARACTERS_PIC = {
   [CHARACTERS.HEAD_ENGINEER]:
     "./assets/sprites/characterPortraits/characters.portraits/general.PNG",
   [CHARACTERS.STABLEMASTER]:
-    "./assets/sprites/characterPortraits/characters.portraits/noble-lady.PNG",
+    "./assets/sprites/characterPortraits/characters.portraits/stablemaster.PNG",
   [CHARACTERS.HEADCHEF]:
-    "./assets/sprites/characterPortraits/characters.portraits/noble-lady.PNG",
+    "./assets/sprites/characterPortraits/characters.portraits/headchef.PNG",
   [CHARACTERS.VISISING_BARRON]:
     "./assets/sprites/characterPortraits/characters.portraits/barron.PNG",
   [CHARACTERS.JESTER]:
-    "./assets/sprites/characterPortraits/characters.portraits/noble-lady.PNG",
+    "./assets/sprites/characterPortraits/characters.portraits/jester.PNG",
   [CHARACTERS.JUDGE]:
-    "./assets/sprites/characterPortraits/characters.portraits/noble-lady.PNG",
+    "./assets/sprites/characterPortraits/characters.portraits/judge.PNG",
   [CHARACTERS.BISHOP]:
-    "./assets/sprites/characterPortraits/characters.portraits/noble-lady.PNG",
+    "./assets/sprites/characterPortraits/characters.portraits/bishop.PNG",
   [CHARACTERS.STEWARD]:
-    "./assets/sprites/characterPortraits/characters.portraits/noble-lady.PNG",
+    "./assets/sprites/characterPortraits/characters.portraits/steward.PNG",
   [CHARACTERS.GENERAL]:
     "./assets/sprites/characterPortraits/characters.portraits/general.PNG",
   [CHARACTERS.MAYOR]:
-    "./assets/sprites/characterPortraits/characters.portraits/noble-lady.PNG",
+    "./assets/sprites/characterPortraits/characters.portraits/mayor.PNG",
   [CHARACTERS.KING]:
     "./assets/sprites/characterPortraits/characters.portraits/king.PNG",
   [CHARACTERS.TUTORIAL_CHARACTER]:
